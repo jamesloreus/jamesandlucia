d0012360cd2683d14768a022a4464412d7079d7b
jamesloreus <jamesloreus@gmail.com>
2026-04-26 13:11:27 -0500

    Add files via upload

---
+++ James and Lucia_files/register2.php
@@ -0,0 +1,17 @@
+var com_adswizz_register_PROTOCOL_VERSION = '2.2.0';
+
+if (typeof com_adswizz_synchro_listenerid === 'undefined') {
+    var com_adswizz_synchro_listenerid = 'ae4497f599c0095570c4bd382293c47f';
+}
+else {
+    com_adswizz_synchro_listenerid = 'ae4497f599c0095570c4bd382293c47f';
+}
+
+if (typeof com_adswizz_synchro_listnerid === 'undefined') {
+    var com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+else {
+    com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+var aw_0_req_gdpr = false;
+var us_privacy = '';
